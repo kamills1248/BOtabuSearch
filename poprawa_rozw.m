9bb403f0cf0785ce41f6b868107f3052bc7e0df5
Zofija <zofija96@gmail.com>
2017-11-29 20:11:17 +0100

    Poprawa poprawy
    pętla po blędach była nie potrzebna, bo będziemy sprawdzać dla każdego błędu inny warunek (ten dotyczący budźetu był tylko dla blad = 1)

--- poprawa_rozw.m
+++ poprawa_rozw.m
@@ -4,18 +4,16 @@ global Bledy; %flagi dopuszczalnosci
 
 for dzien = 1:5
     for slot = 1:3
-       for blad = 1:3
-           if(Bledy(slot,dzien,blad) ~= 0)
-               r = rozwiazanie(slot, dzien*2-1);   % r-ta restauracja
-               k = rozwiazanie(slot, dzien*2);  % k-ty zestaw
-               while(R(r,2*k+2) == 1000)
-                   rozwiazanie(slot,dzien*2-1)=randi([1 10], 1);
-                   rozwiazanie(slot,dzien*2)=randi([1 10], 1);
-                   r = rozwiazanie(slot, dzien*2-1);   % r-ta restauracja
-                   k = rozwiazanie(slot, dzien*2);  % k-ty zestaw
-               end               
-           end
-       end
+        r = rozwiazanie(slot, dzien*2-1);   % r-ta restauracja
+        k = rozwiazanie(slot, dzien*2);  % k-ty zestaw
+        if(Bledy(slot,dzien,1) ~= 0) % tylko dla budzetu sprawdzamy warunek <1000
+            while(R(r,2*k+2) == 1000)
+                rozwiazanie(slot,dzien*2-1)=randi([1 10], 1);
+                rozwiazanie(slot,dzien*2)=randi([1 10], 1);
+                r = rozwiazanie(slot, dzien*2-1);   % r-ta restauracja
+                k = rozwiazanie(slot, dzien*2);  % k-ty zestaw
+            end
+        end
     end   
 end
 end
