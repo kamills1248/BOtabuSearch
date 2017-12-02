47606bfc4cf7a4bca666ee4ec5f4f24219673c4e
Zofija <zofija96@gmail.com>
2017-12-02 16:10:21 +0100

    Dodanie zmiennej
    Mala zmiana - dodanie zmiennej cena_s jako cena posilku w slocie, zeby potem sprawdzac czy sie nie zwiekszyla (byl tu wczesniej blad)

--- poprawa_rozw.m
+++ poprawa_rozw.m
@@ -35,12 +35,14 @@ for dzien = 1:5
         
         % jesli ten posilek jest najdrozszym, to zamieniamy do na tanszy
         % (jezli jest pare tak samo drogich, to wszystkie zmniejszymy)
-        if (R(r,2*k+2) == cena)
-            while(R(r,2*k+2) >= cena) %dopoki cena sie nie zmiejszy
+        cena_s = R(r,2*k+2); % cena w danym slocie
+        if (cena_s == cena)
+            while(cena_s >= cena) %dopoki cena sie nie zmiejszy
                 rozwiazanie(slot,dzien*2-1)= randi([1 10], 1);
                 rozwiazanie(slot,dzien*2)= randi([1 10], 1);
                 r = rozwiazanie(slot, dzien*2-1);   % r-ta restauracja
                 k = rozwiazanie(slot, dzien*2);  % k-ty zestaw
+                cena_s = R(r,2*k+2);
             end
         end
         
@@ -50,7 +52,7 @@ for dzien = 1:5
         czas_zuzyty = Z(2,k)+ R(r,2*k+3) + do_rest + od_rest;
         if(Bledy(slot,dzien,2) ~= 0 && czas_zuzyty > S(slot,dzien) + 15) 
             %jezeli w tablicy bledow byl tutaj blad i nadal jest po zmianach w budzecie
-            while(czas_zuzyty >= (S(slot,dzien) + 15) || R(r,2*k+2) >= cena) %dopoki czas sie nie zmniejszy i cena bedzie taka jak byla lub mniejsza
+            while(czas_zuzyty >= (S(slot,dzien) + 15) || R(r,2*k+2) >= cena_s) %dopoki czas sie nie zmniejszy i cena bedzie taka jak byla lub mniejsza
                 rozwiazanie(slot,dzien*2-1)= randi([1 10], 1);
                 rozwiazanie(slot,dzien*2)= randi([1 10], 1);
                 r = rozwiazanie(slot, dzien*2-1);   % r-ta restauracja
@@ -65,7 +67,7 @@ for dzien = 1:5
          energia = energia - sp*przerwa(slot+1) + Z(1,k);
          if(Bledy(slot,dzien,3) ~= 0 && (energia < 0 || energia > E_max)) 
              %jezeli w tablicy bledow byl tutaj blad i nadal jest po powyuzszych zmianach
-             while(energia > E_max || energia < 0 || czas_zuzyty <= (S(slot,ceil(dzien/2)) + 15) || R(r,2*k+2) <= cena) 
+             while(energia > E_max || energia < 0 || czas_zuzyty <= (S(slot,ceil(dzien/2)) + 15) || R(r,2*k+2) <= cena_s) 
                  %dopoki energia sie nie zgadza i czas i cena beda rowne lub mniejsze poprzednim
                  rozwiazanie(slot,dzien*2-1)= randi([1 10], 1);
                  rozwiazanie(slot,dzien*2)= randi([1 10], 1);
