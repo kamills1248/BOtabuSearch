7de9debab3587041b3fc5552b746f283948bbb3f
Peter <p.golyzniak@gmail.com>
2017-12-07 13:12:47 +0100

    zuniwersalizowanie losuj_i_popraw

--- losuj_i_popraw.m
+++ losuj_i_popraw.m
@@ -2,9 +2,28 @@ function [ poprawione, wynik ] = losuj_i_popraw( )
 global Bledy; %flagi dopuszczalnosci
 Bez_bledow = zeros(3,5,3);
 
+%uniwersalne rozmiary macierzy
+global ilosc_zestawow; global ilosc_rest;
+
 %% Losujemy rozwiazanie poczatkowe
-rozw = randi([1 10], 3, 10);
-fc(rozw); %obliczamy fc I USTAWIAMY FLAGI BLEDOW; nie chcemy wyniku
+rozw = zeros(3,10); % 3 sloty na dzien, 5 restauracji i 5 zestawow
+
+for dzien = 1:5
+    for slot = 1:3 
+        % losowanie restauracji:
+        rozw(slot, dzien*2-1) = randi([1 ilosc_rest], 1);
+        % losowanie zestawu:
+        rozw(slot, dzien*2) = randi([1 ilosc_zestawow], 1);
+    end
+end
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% to ponizej zostawiam, bo jakby cos wyzej nie dzialalo, to to ponizej juz
+% dzialalo
+% rozw = randi([1 10], 3, 10);
+% fc(rozw); %obliczamy fc I USTAWIAMY FLAGI BLEDOW; nie chcemy wyniku
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
 
 %% Poprawa rozwiazania
 [ poprawione, wynik ] = poprawa_rozw(rozw);
@@ -14,7 +33,15 @@ fc(rozw); %obliczamy fc I USTAWIAMY FLAGI BLEDOW; nie chcemy wyniku
 if (~isequal(Bledy, Bez_bledow))
     i = 0;
     while(~isequal(Bledy, Bez_bledow) && i < 20)
-        rozw = randi([1 10], 3, 10);
+        % losowanie kolejnego rozwiazania:
+        for dzien = 1:5
+            for slot = 1:3 
+                % losowanie restauracji:
+                rozw(slot, dzien*2-1) = randi([1 ilosc_rest], 1);
+                % losowanie zestawu:
+                rozw(slot, dzien*2) = randi([1 ilosc_zestawow], 1);
+            end
+        end
         [ poprawione, wynik] = poprawa_rozw(rozw);
         i = i + 1;
     end
