72fb3b37102d1bae5813d6b3c2e5c2f3d3d5639b
Zofija <zofija96@gmail.com>
2017-12-14 16:04:59 +0100

    kolejne dane do ew zmiany w plikach
    + male poprawki

--- wczytaj_dane_z_pliku.m
+++ wczytaj_dane_z_pliku.m
@@ -1,3 +1,5 @@
+clear all;
+close all;
 global R; global Z; global D; global poz_cz; global B; global S; global Ep;
 global E_max;
 
@@ -40,12 +42,10 @@ Ep = 700;
 % "pelnosc brzucha"
 E_max = 1000; %energia, ktora maksymalnie na raz mozna miec w sobie
 
-% sloty czasowe
-%      dz1     dz2     dz3     dz4     dz5
-S = [   30,     20,     25,     15,     30 ;  %slot 1 od 10
-        45,     60,     90,     50,     60 ;  %slot 2 od 13
-        15,     20,     15,     35,     15];  %slot 3 od 16:30
-     
+% sloty czasowe - dlogosci (kolumny: kolejne dni, wiersze: sloty kolejno: 
+% slot 1 od 10, slot 2 od 13, slot 3 od 16:30)
+load S.txt - ascii
+    
 % macierz odleglosci knajp od budynkow z zajeciami
 % restauracje \ budynki
 
@@ -59,9 +59,9 @@ D = [(abs(R(:,1) - poz_bud(1,1)) + abs(R(:,2) - poz_bud(1,2))),...
 %  polozenie czlowieka przed i po slocie (kolumny: kolejne dni, wiersze:
 %  polozenie kolejno: przed slot 1 przed 10, po slot 1 przed 13, po slot 2
 %  po 13, po slot 3 po 16:30)
-% load poz_cz.txt -ascii  % NIE DZIALA, TRZEBA WYMYSLIC COS INNEGO!!!
+load poz_cz.txt -ascii % UWAGA: budynki okreslone sa za pomoca nr tak jak zdefiniowano w linii 16!!!
 
-         
+
 % licze ilosc dni
 size_poz_cz = size(poz_cz);
 ilosc_dni = size_poz_cz(2);
