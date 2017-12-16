a240187ee988c68939da027e38b227491f2c31cc
Peter <p.golyzniak@gmail.com>
2017-12-16 11:44:39 +0100

    zmiana współrzędnych (przeskalowanie)
    dopasowanie współrzędnych do mapy

--- dane_testowe.m
+++ dane_testowe.m
@@ -1,29 +1,29 @@
 global R; global Z; global D; global poz_cz; global B; global S; global Ep;
-global E_max;
+global E_max; global poz_bud;
 
 %uniwersalne rozmiary macierzy
 global ilosc_zestawow; global ilosc_rest; global ilosc_dni;
 
-% polozenie czlowieka 
-poz_bud = [  9, 3.5;
-            14, 3.5;
-            13,   4;
-            12,   4;
-             2, 1.5]; %B1, C1, C2, C3, D8
+% nowe wspolzedne budynkow na planie
+poz_bud = [ 618, 154;  %B1
+            795, 145;  %C1
+            763, 115;  %C2
+            723, 113;  %C3
+            190, 271]; %D8
          
 B1 = 1; C1 = 2; C2 = 3; C3 = 4; D8 = 5;
 
 %       x   y    ocena    c1    t1    c2    t2   c3   t3    c4    t4  
-r1 = [   5,  5.5,   8,    25,   20,   16,   15, 1000, 100, 1000,  100;  %Dagrasso
-         7,  1.5,   6,  1000,  100,    8,    2,   10,   3,   10,   3 ;  %Krakus
-       7.5,    3,   7,  1000,  100,   10,    5,    9,   2,    9,   2 ;  %Tawo
-       8.5,  1.5,   4,  1000,  100,    9,    3,   11,   4,   11,   4 ;  %Nawojka
-         9,  3.5,   3,  1000,  100,    7,    8,   15,   5, 1000, 100 ;  %Labirynt
-         9,  5.5,   8,  1000,  100,   15,    5, 1000, 100, 1000, 100 ;  %PastaMasta
-        10,  5.5,   7,    22,   20, 1000,  100, 1000, 100, 1000, 100 ;  %Kebab
-        11,  4.5,   8,    30,   15,   20,    8,   15,   8,   16,   7 ;  %Lemon
-        12,    0,   4,  1000,  100,   11,    4,   12,   5,   12,   4 ;  %Zaczek
-        13,    4,   2,  1000,  100, 1000,  100,    8,   6,   10,   7];  %Barek
+r1 = [ 352,   93,   8,    25,   20,   16,   15, 1000, 100, 1000,  100;  %Dagrasso
+       519,  260,   6,  1000,  100,    8,    2,   10,   3,   10,   3 ;  %Krakus
+       526,  217,   7,  1000,  100,   10,    5,    9,   2,    9,   2 ;  %Tawo
+       584,  277,   4,  1000,  100,    9,    3,   11,   4,   11,   4 ;  %Nawojka
+       616,  175,   3,  1000,  100,    7,    8,   15,   5, 1000, 100 ;  %Labirynt
+       604,   47,   8,  1000,  100,   15,    5, 1000, 100, 1000, 100 ;  %PastaMasta
+       659,   51,   7,    22,   20, 1000,  100, 1000, 100, 1000, 100 ;  %Kebab
+       693,   99,   8,    30,   15,   20,    8,   15,   8,   16,   7 ;  %Lemon
+       851,  352,   4,  1000,  100,   11,    4,   12,   5,   12,   4 ;  %Zaczek
+       779,  115,   2,  1000,  100, 1000,  100,    8,   6,   10,   7];  %Barek
         
 %       c5     t5   c6    t6   c7    t7    c8    t8   c9   t9   c10   t10
 r2 = [ 1000,  100,    5,   2,    7,   5,    6,    4, 1000, 100, 1000, 100  ;  %Dagrasso
@@ -36,7 +36,6 @@ r2 = [ 1000,  100,    5,   2,    7,   5,    6,    4, 1000, 100, 1000, 100  ;  %D
        1000,  100,    5,   3,    7,   4, 1000,  100, 1000, 100,    4,   1  ;  %Lemon
        1000,  100,    3,   2, 1000, 100, 1000,  100,    9,   4, 1000, 100  ;  %Zaczek
        1000,  100,    4,   3, 1000, 100,    8,    5,    9,   8,    3,   1 ];  %Barek
-    
         
 R = [r1, r2];
 
@@ -74,12 +73,16 @@ S = [   30,     20,     25,     15,     30 ;  %slot 1 od 10
 % macierz odleglosci knajp od budynkow z zajeciami
 % restauracje \ budynki
 
- 
+% wiersze to restauracje, kolumny, to budynki 
 D = [(abs(R(:,1) - poz_bud(1,1)) + abs(R(:,2) - poz_bud(1,2))),...
      (abs(R(:,1) - poz_bud(2,1)) + abs(R(:,2) - poz_bud(2,2))),...
      (abs(R(:,1) - poz_bud(3,1)) + abs(R(:,2) - poz_bud(3,2))),...
      (abs(R(:,1) - poz_bud(4,1)) + abs(R(:,2) - poz_bud(4,2))),...
      (abs(R(:,1) - poz_bud(5,1)) + abs(R(:,2) - poz_bud(5,2)))];
+
+% dziele przez 60, poniewaz mapa ma rozmiar 360x900 a zalozylismy, ze
+% przejscie jej z zachodu na wschod zajmuje 15 minut, no i 900/15=60 
+D = D./60;
  
 %  polozenie przed i po slocie
 %            dz1     dz2     dz3     dz4     dz5
